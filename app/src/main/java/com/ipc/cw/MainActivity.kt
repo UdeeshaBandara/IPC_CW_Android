fdaf6b2b68ae672d384983c93188a1de9914291e
udeeshabandara <udeeshabandara@gmail.com>
2023-04-19 06:46:47 +0530

    feat: added car select bottom sheet

--- app/src/main/java/com/ipc/cw/MainActivity.kt
+++ app/src/main/java/com/ipc/cw/MainActivity.kt
@@ -1,23 +1,22 @@
 package com.ipc.cw
 
-import android.graphics.Color
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.view.WindowInsets
-import android.view.WindowManager
+import android.view.*
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.maps.CameraUpdateFactory
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.firebase.database.*
 import com.google.firebase.firestore.*
-import com.google.firebase.messaging.FirebaseMessaging
-
 import com.ipc.cw.model.Car
 import com.ipc.cw.model.Location
 
@@ -32,6 +31,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     var authReference: CollectionReference? = null
     var carList  = mutableListOf<Car>()
     var locationList = mutableListOf<List<Location>>()
+    lateinit var recyclerCar: RecyclerView
+    lateinit var carBottomSheet: CarBottomSheet
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,39 +58,118 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         database = FirebaseFirestore.getInstance()
         authReference = database!!.collection("carsList")
 
-        authReference!!.addSnapshotListener(EventListener { carListSnapShot, e ->
-            if (e != null) {
-                return@EventListener
-            }
+        authReference!!
+            .get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    carList = task.result.toObjects(Car::class.java)
 
-
-            carList  = carListSnapShot!!.toObjects(Car::class.java)
-            for (queryDocumentSnapshot in carList) {
-
-                authReference!!.document(queryDocumentSnapshot.uID!!).collection("locationList").addSnapshotListener { snapshotNested, e ->
-
-                    locationList.add(snapshotNested!!.toObjects(Location::class.java))
-                    var ww  = snapshotNested!!.toObjects(Location::class.java)
-
-                    map?.addPolyline(
-                        PolylineOptions().add( LatLng(ww[0].location!!.latitude,ww[0].location!!.longitude), LatLng(ww[1].location!!.latitude,ww[1].location!!.longitude),  LatLng(ww[0].location!!.latitude,ww[0].location!!.longitude))
-                            .width
-                                (12f)
-                            .color(Color.RED)
-                            .geodesic(true)
-                    )
-                    map?.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(6.971312953677447, 79.97690780715868), 13f))
+                    Log.e("hiiiiii", carList[0].uID!!)
 
                 }
-
             }
 
-        })
+//        authReference!!.addSnapshotListener(EventListener { carListSnapShot, e ->
+//            if (e != null) {
+//                return@EventListener
+//            }
+//
+//
+//            carList  = carListSnapShot!!.toObjects(Car::class.java)
+//            for (queryDocumentSnapshot in carList) {
+//
+//                authReference!!.document(queryDocumentSnapshot.uID!!).collection("locationList").addSnapshotListener { snapshotNested, e ->
+//
+//                    locationList.add(snapshotNested!!.toObjects(Location::class.java))
+//                    var ww  = snapshotNested!!.toObjects(Location::class.java)
+//
+//                    map?.addPolyline(
+//                        PolylineOptions().add( LatLng(ww[0].location!!.latitude,ww[0].location!!.longitude), LatLng(ww[1].location!!.latitude,ww[1].location!!.longitude),  LatLng(ww[0].location!!.latitude,ww[0].location!!.longitude))
+//                            .width
+//                                (12f)
+//                            .color(Color.RED)
+//                            .geodesic(true)
+//                    )
+//                    map?.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(6.971312953677447, 79.97690780715868), 13f))
+//
+//                }
+//
+//            }
+//
+//        })
+
+        findViewById<TextView>(R.id.select_car).setOnClickListener {
+            carBottomSheet = CarBottomSheet(this)
+            carBottomSheet.setContentView(R.layout.bottom_sheet_car)
+            carBottomSheet.show()
+        }
 
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         this.map = googleMap
 
+    }
+
+    inner class CarBottomSheet(context: Context) :
+        BottomSheetDialog(context) {
+
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            recyclerCar = this.findViewById(R.id.recycler_cars)!!
+
+
+            this.findViewById<ImageView>(R.id.btn_clear)!!.setOnClickListener {
+                carBottomSheet.dismiss()
+
+            }
+
+            recyclerCar.adapter = DistrictAdapter()
+            recyclerCar.layoutManager = LinearLayoutManager(
+                applicationContext,
+                LinearLayoutManager.VERTICAL,
+                false
+            )
+
+
+        }
+    }
+
+    private inner class CarViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var car_name: TextView = itemView.findViewById(R.id.car_name)
+
+
+    }
+
+    private inner class DistrictAdapter : RecyclerView.Adapter<CarViewHolder>() {
+
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarViewHolder {
+            val view: View = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_car, parent, false)
+
+            return CarViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return carList.size
+        }
+
+        @SuppressLint("SetTextI18n")
+        override fun onBindViewHolder(holder: CarViewHolder, position: Int) {
+            try {
+                holder.car_name.text =
+                    "${carList[position].carNo} ${carList[position].driverName}"
+
+
+                holder.car_name.setOnClickListener {
+
+
+                }
+            } catch (e: Exception) {
+                e.stackTrace
+            }
+        }
     }
 }
