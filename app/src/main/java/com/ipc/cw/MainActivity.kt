3ee886cb36fd31145971dc1a85294470e64461d4
udeeshabandara <udeeshabandara@gmail.com>
2023-04-20 19:19:58 +0530

    fix: updated bottom sheet card view

--- app/src/main/java/com/ipc/cw/MainActivity.kt
+++ app/src/main/java/com/ipc/cw/MainActivity.kt
@@ -101,11 +101,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             recyclerCar = this.findViewById(R.id.recycler_cars)!!
 
 
-            this.findViewById<ImageView>(R.id.btn_clear)!!.setOnClickListener {
-                carBottomSheet.dismiss()
-
-            }
-
             recyclerCar.adapter = DistrictAdapter()
             recyclerCar.layoutManager = LinearLayoutManager(
                 applicationContext,
@@ -119,6 +114,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private inner class CarViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var car_name: TextView = itemView.findViewById(R.id.car_name)
+        var driver_name: TextView = itemView.findViewById(R.id.driver_name)
 
 
     }
@@ -141,7 +137,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         override fun onBindViewHolder(holder: CarViewHolder, position: Int) {
             try {
                 holder.car_name.text =
-                    "${carList[position].carNo} ${carList[position].driverName}"
+                    "${carList[position].carNo}"
+                holder.driver_name.text =
+                    "${carList[position].driverName}"
 
 
                 holder.car_name.setOnClickListener {
@@ -195,35 +193,26 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             }
     }
     private fun bitmapFromVector(context: Context, vectorResId: Int): BitmapDescriptor? {
-        // below line is use to generate a drawable.
+
         val vectorDrawable = ContextCompat.getDrawable(
             context, vectorResId
         )
 
-        // below line is use to set bounds to our vector
-        // drawable.
         vectorDrawable!!.setBounds(
             0, 0, vectorDrawable.intrinsicWidth,
             vectorDrawable.intrinsicHeight
         )
 
-        // below line is use to create a bitmap for our
-        // drawable which we have added.
         val bitmap = Bitmap.createBitmap(
             vectorDrawable.intrinsicWidth,
             vectorDrawable.intrinsicHeight,
             Bitmap.Config.ARGB_8888
         )
 
-        // below line is use to add bitmap in our canvas.
         val canvas = Canvas(bitmap)
 
-        // below line is use to draw our
-        // vector drawable in canvas.
         vectorDrawable.draw(canvas)
 
-        // after generating our bitmap we are returning our
-        // bitmap.
         return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
 }
